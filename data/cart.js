04cea93218ebb40dcbefeb0065eb20aaa126d533
nuova <kanmengnuo@outlook.com>
2025-03-21 16:52:31 +0800

    Fix price formating and radio selectors. Delete product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export let cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
+
 export function addToCart(productId){
   let matchingItem;
 
@@ -16,4 +23,16 @@ export function addToCart(productId){
       quantity: 1
     });
   }
+}
+
+export function removeFromCart(productId){
+  const newCart = [];
+
+  cart.forEach((cartItem)=>{
+    if(productId !== cartItem.productId){
+      newCart.push(cartItem);
+    }
+  });
+  
+  cart = newCart;
 }
