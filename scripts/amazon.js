67997655368d64972d9ea665e5c956fb5cb73a23
nuova <kanmengnuo@outlook.com>
2025-03-28 18:36:52 +0800

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,80 +1,84 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-let productHTML = '';
+loadProducts(renderProductsGrid);
 
-products.forEach((product)=>{
-  productHTML+=`
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
+function renderProductsGrid(){
+  let productHTML = '';
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  products.forEach((product)=>{
+    productHTML+=`
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
-
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
-
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      ${product.extraInfoHTML()}
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
-});
-
-function updateCartQuantity(){
-  let cartQuantity = 0;/*因为下面forEach循环做的是从头累加购物车中的数量，每点击button一次就从头算一次*/
-
-  cart.forEach((cartItem)=>{
-    cartQuantity += cartItem.quantity;
-  });/*update the web page, has nothing to do with the cart*/
-  
-  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-}
-
-document.querySelector('.js-products-grid').innerHTML = productHTML;
-
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-  button.addEventListener('click',() => {
-    const productId = button.dataset.productId;
-    addToCart(productId);
-    updateCartQuantity();
+    `;
   });
-});
+
+  function updateCartQuantity(){
+    let cartQuantity = 0;/*因为下面forEach循环做的是从头累加购物车中的数量，每点击button一次就从头算一次*/
+
+    cart.forEach((cartItem)=>{
+      cartQuantity += cartItem.quantity;
+    });/*update the web page, has nothing to do with the cart*/
+    
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  }
+
+  document.querySelector('.js-products-grid').innerHTML = productHTML;
+
+  document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',() => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
+}
